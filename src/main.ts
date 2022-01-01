2150afafd86efc0f411b9077ac087af416a8ca4b
Sho-ki <shoki0116.highjump@gmail.com>
2022-01-01 14:29:19 +0900

    add cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 require('dotenv').config();
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.use(cors());
+
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
