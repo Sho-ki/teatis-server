32f41a1ab77d4f1e2e15b4d57cc9530ea7e8d759
Sho-ki <shoki0116.highjump@gmail.com>
2022-01-07 01:46:14 +0900

    write unit test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "You need to add endpoint"', () => {
+      expect(appController.getHello()).toBe('You need to add endpoint');
     });
   });
 });
