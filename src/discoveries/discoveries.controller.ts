6081c7ac011f10f2b4e5d45f1fc553b6c3d311b7
Sho-ki <shoki0116.highjump@gmail.com>
2022-01-14 00:52:11 +0900

    use uuid when jumping to typeform

--- src/discoveries/discoveries.controller.ts
+++ src/discoveries/discoveries.controller.ts
@@ -2,11 +2,13 @@ import {
   Controller,
   Get,
   Query,
+  Redirect,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateDiscoveryInfoDto } from './dtos/create-discovery.dto';
 import { DiscoveriesService } from './discoveries.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('discovery')
 @UsePipes(new ValidationPipe({ transform: true }))
@@ -16,5 +18,15 @@ export class DiscoveriesController {
   @Get()
   createDiscovery(@Query() body: CreateDiscoveryInfoDto) {
     return this.discoveriesService.createDiscovery(body);
+  }
+
+  @Get('typeform')
+  @Redirect()
+  redirectToTypeform() {
+    const uuid = uuidv4();
+    const TYPEFORM_URL_ID = process.env.TYPEFORM_URL_ID;
+    return {
+      url: `https://teatis.typeform.com/to/${TYPEFORM_URL_ID}#typeformid=${uuid}`,
+    };
   }
 }
