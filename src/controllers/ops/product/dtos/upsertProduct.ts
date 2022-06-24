a6b4785d9252461d10722071b7858d0d61482d97
Sho-ki <shoki0116.highjump@gmail.com>
2022-06-25 01:50:38 +0900

    solve conflict

---
+++ src/controllers/ops/product/dtos/upsertProduct.ts
@@ -0,0 +1,109 @@
+import {
+  IsArray,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class UpsertProductDto {
+  @IsString()
+  @IsOptional()
+  activeStatus?: 'active' | 'inactive';
+
+  @IsString()
+  @IsOptional()
+  preservationStyle?: 'normal' | 'refrigerated' | 'frozen';
+
+  @IsString()
+  @IsOptional()
+  allergenLabel?: string;
+
+  @IsString()
+  @IsOptional()
+  ingredientLabel?: string;
+
+  @IsString()
+  @IsOptional()
+  expertComment?: string;
+
+  @IsNumber()
+  @IsOptional()
+  WSP?: number;
+
+  @IsNumber()
+  @IsOptional()
+  MSP?: number;
+
+  @IsString()
+  label: string;
+
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsNumber()
+  productProviderId: number;
+
+  @IsString()
+  @IsOptional()
+  upcCode?: string;
+
+  @IsNumber()
+  @IsOptional()
+  flavorId?: number;
+
+  @IsNumber()
+  @IsOptional()
+  categoryId?: number;
+
+  @IsNumber()
+  @IsOptional()
+  vendorId?: number;
+
+  @IsString()
+  externalSku: string;
+
+  @IsArray()
+  @IsOptional()
+  allergenIds?: number[];
+
+  @IsArray()
+  @IsOptional()
+  foodTypeIds?: number[];
+
+  @IsArray()
+  @IsOptional()
+  images?: { src: string; position: number }[];
+
+  @IsArray()
+  @IsOptional()
+  ingredientIds?: number[];
+
+  @IsArray()
+  @IsOptional()
+  cookingMethodIds?: number[];
+
+  @IsObject()
+  nutritionFact: {
+    quantity: number;
+    servingSize: number;
+    calories: number;
+    totalFat: number;
+    saturatedFat: number;
+    transFat: number;
+    cholesterole: number;
+    sodium: number;
+    totalCarbohydrate: number;
+    dietaryFiber: number;
+    totalSugar: number;
+    addedSugar: number;
+    protein: number;
+    sweet: number;
+    sour: number;
+    salty: number;
+    bitter: number;
+    spicy: number;
+    texture: string;
+  };
+}
