795e921b61ac99b71eef62f84c42d1106fa9fe15
Sho-ki <shoki0116.highjump@gmail.com>
2022-07-21 05:45:37 +0900

    env test

--- src/repositories/klaviyo/klaviyo.repository.spec.ts
+++ src/repositories/klaviyo/klaviyo.repository.spec.ts
@@ -23,7 +23,7 @@ describe('Klaviyo test', () => {
     const email = "jestEmail@test.com";
     const customerUuid = "9f90c1c1-7481-4d4f-a6af-2d06515eb3f8"
     const recommendBoxType = "HC"
-    const serverSideUrl = "https://a.klaviyo.com/api/v2/list/" + process.env.KLAVIYO_POTENTIAL_CUSTOMER_LIST + "/members?api_key="+process.env.KLAVIYO_API
+    const serverSideUrl = `https://a.klaviyo.com/api/v2/list/${process.env.KLAVIYO_POTENTIAL_CUSTOMER_LIST}/members?api_key=${process.env.KLAVIYO_API}`
 
     describe('post user information to klaviyo', () => {
         it('should update the klaviyo list successfully', async () => {
