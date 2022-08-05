3f68ae10b35a5dcbd29264e148451cc4c0b379de
Sho-ki <shoki0116.highjump@gmail.com>
2022-08-06 01:34:09 +0900

    add db flow in cloudrun yml

--- src/repositories/teatisDB/product/productGeneral.repository.spec.ts
+++ src/repositories/teatisDB/product/productGeneral.repository.spec.ts
@@ -5,33 +5,32 @@ import { ProductGeneralRepository } from './productGeneral.repository';
 
 describe('productGeneral', () => {
   describe('with existing posts', () => {
-    it('passes', () => {});
-    // beforeEach(() => {
-    //   setUpPrismaClient();
-    // });
+    beforeEach(() => {
+      setUpPrismaClient();
+    });
 
-    // afterEach(async () => {
-    //   await cleanUpPrismaClient();
-    // });
+    afterEach(async () => {
+      await cleanUpPrismaClient();
+    });
 
-    // describe('#getExistingProductIngredients', () => {
-    //   beforeEach(async () => {
-    //     await prismaForTest.product.create({
-    //       data: {
-    //         name: 'test',
-    //         label: 'test',
-    //         externalSku: '1234',
-    //         productNutritionFact: { create: { calories: 200 } },
-    //         productProvider: { create: { provider: 'test_provider' } },
-    //       },
-    //     });
-    //   });
+    describe('#getExistingProductIngredients', () => {
+      beforeEach(async () => {
+        await prismaForTest.product.create({
+          data: {
+            name: 'test',
+            label: 'test',
+            externalSku: '1234',
+            productNutritionFact: { create: { calories: 200 } },
+            productProvider: { create: { provider: 'test_provider' } },
+          },
+        });
+      });
 
-    //   it('retrieves product objects that matches the sku', async () => {
-    //     const repository = new ProductGeneralRepository(prismaForTest as PrismaService);
-    //     const [products] = await repository.getProductsBySku({ products: [{ sku: '1234' }] });
-    //     expect(products.length).toEqual(1);
-    //   });
-    // });
+      it('retrieves product objects that matches the sku', async () => {
+        const repository = new ProductGeneralRepository(prismaForTest as PrismaService);
+        const [products] = await repository.getProductsBySku({ products: [{ sku: '1234' }] });
+        expect(products.length).toEqual(1);
+      });
+    });
   });
 });
