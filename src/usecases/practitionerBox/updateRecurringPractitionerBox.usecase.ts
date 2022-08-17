a3c157c01b5204789ff0884ae8e71339e85991c1
筒井裕 <hiroshi.tsutsui415@gmail.com>
2022-08-17 10:57:50 -0400

    update practitioner with no duplicate products

--- src/usecases/practitionerBox/updateRecurringPractitionerBox.usecase.ts
+++ src/usecases/practitionerBox/updateRecurringPractitionerBox.usecase.ts
@@ -1,19 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CreatePractitionerBoxDto } from '@Controllers/discoveries/dtos/createPractitionerBox';
+import { ReturnValueType } from '@Filters/customError';
+import { PractitionerAndBox } from '@Domains/PractitionerAndBox';
+import { PractitionerBoxRepositoryInterface } from '@Repositories/teatisDB/practitioner/practitionerBox.repo';
 
-// export interface updateRecurringPractitionerBoxUsecaseInterface {
-//     updateRecurringPractitionerBox({
-//       practitionerId,
-//       products,
-//       label,
-//       description,
-//       note,
-//     }: CreatePractitionerBoxDto): Promise<ReturnValueType<PractitionerAndBox>>;
-// }
+export interface updateRecurringPractitionerBoxUsecaseInterface {
+  updateRecurringPractitionerBox(
+    recurringProducts: CreatePractitionerBoxDto['products'],
+    practitionerDetails: CreatePractitionerBoxDto,
+  ): Promise<ReturnValueType<PractitionerAndBox>>;
+}
 
 @Injectable()
 export class UpdateRecurringPractitionerBoxUsecase
-// implements updateRecurringPractitionerBoxUsecaseInterface
+implements updateRecurringPractitionerBoxUsecaseInterface
 {
-  constructor() {}
-//   async updateRecurringPractitionerBox() {}
+  constructor(
+    @Inject('PractitionerBoxRepositoryInterface')
+    private practitionerBoxRepository: PractitionerBoxRepositoryInterface,
+  ) {}
+  async updateRecurringPractitionerBox(
+    recurringProducts,
+    practitionerDetails,
+  ): Promise<ReturnValueType<PractitionerAndBox>>{
+    const { practitionerId, practitionerBoxUuid, label, products, description, note } = practitionerDetails;
+    const chosenProductIds: number[] = practitionerDetails.products.map(product => product.id);
+    const recurringProductIds: number[] = recurringProducts.map(recurringProduct => recurringProduct.id);
+    const lastChosenProducts: number[] =
+      recurringProductIds.filter(recurringProduct => chosenProductIds.includes(recurringProduct));
+    if (!lastChosenProducts.length) {
+      const [practitionerBoxProduct, createPractitionerBoxProductError] =
+        await this.practitionerBoxRepository.upsertPractitionerAndPractitionerBox({
+          practitionerId,
+          practitionerBoxUuid,
+          label,
+          products,
+          description,
+          note,
+        });
+      if(createPractitionerBoxProductError){
+        return [undefined, createPractitionerBoxProductError];
+      }
+      return [practitionerBoxProduct, undefined];
+    }
+    return [undefined, undefined];
+  }
 }
