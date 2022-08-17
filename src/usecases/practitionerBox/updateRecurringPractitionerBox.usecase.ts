7878e685f2aef83ab73ac340e4e13c455e87e41e
筒井裕 <hiroshi.tsutsui415@gmail.com>
2022-08-17 01:49:01 -0400

    usecase skeleton

---
+++ src/usecases/practitionerBox/updateRecurringPractitionerBox.usecase.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+
+// export interface updateRecurringPractitionerBoxUsecaseInterface {
+//     updateRecurringPractitionerBox({
+//       practitionerId,
+//       products,
+//       label,
+//       description,
+//       note,
+//     }: CreatePractitionerBoxDto): Promise<ReturnValueType<PractitionerAndBox>>;
+// }
+
+@Injectable()
+export class UpdateRecurringPractitionerBoxUsecase
+// implements updateRecurringPractitionerBoxUsecaseInterface
+{
+  constructor() {}
+//   async updateRecurringPractitionerBox() {}
+}
