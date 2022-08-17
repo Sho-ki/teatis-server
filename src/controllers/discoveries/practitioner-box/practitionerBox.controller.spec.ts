2baf59b91a7be251c0ff9126ecb37be3bd244420
筒井裕 <hiroshi.tsutsui415@gmail.com>
2022-08-17 07:26:06 -0400

    fix test

--- src/controllers/discoveries/practitioner-box/practitionerBox.controller.spec.ts
+++ src/controllers/discoveries/practitioner-box/practitionerBox.controller.spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CreatePractitionerBoxUsecaseInterface } from '@Usecases/practitionerBox/createPractitionerBox.usecase';
 import { GetPractitionerBoxByLabelUsecaseInterface } from '@Usecases/practitionerBox/getPractitionerBoxByLabel.usecase';
 import { GetPractitionerBoxByUuidUsecaseInterface } from '@Usecases/practitionerBox/getPractitionerBoxByUuid.usecase';
-import { GetRecurringPractitionerBoxUsecase } from '@Usecases/practitionerBox/getRecurringPractitionerBox.usecase';
+import { GetRecurringPractitionerBoxUsecaseInterface } from '@Usecases/practitionerBox/getRecurringPractitionerBox.usecase';
 import { PractitionerAndBox } from '@Domains/PractitionerAndBox';
 import { PractitionerBoxController } from './practitionerBox.controller';
 import { ReturnValueType } from '@Filters/customError';
@@ -48,7 +48,10 @@ describe('PractitionerBoxController', () => {
         },
         {
           provide: 'GetRecurringPractitionerBoxUsecaseInterface',
-          useClass: GetRecurringPractitionerBoxUsecase,
+          useValue: {
+            getRecurringPractitionerBox: () =>
+              Promise.resolve<unknown>([mockBox]),
+          } as GetRecurringPractitionerBoxUsecaseInterface,
         },
       ],
     }).compile();
