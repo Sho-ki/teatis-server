03a3d0d43d9a6cb526ca70338a7e9e100147f069
筒井裕 <hiroshi.tsutsui415@gmail.com>
2022-08-23 21:09:56 -0400

    fix test

--- src/controllers/discoveries/practitioner-box/practitionerBox.controller.spec.ts
+++ src/controllers/discoveries/practitioner-box/practitionerBox.controller.spec.ts
@@ -1,15 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { CreatePractitionerBoxUsecaseInterface } from '@Usecases/practitionerBox/createPractitionerBox.usecase';
-import { GetAllPractitionerBoxesUsecaseInterface } from '@Usecases/practitionerBox/getAllPractitionerBoxes.usecase';
-import { GetAllProductsUsecaseInterface } from '@Usecases/product/getAllProducts.usecase';
 import { GetPractitionerBoxByLabelUsecaseInterface } from '@Usecases/practitionerBox/getPractitionerBoxByLabel.usecase';
 import { GetPractitionerBoxByUuidUsecaseInterface } from '@Usecases/practitionerBox/getPractitionerBoxByUuid.usecase';
 import { GetRecurringPractitionerBoxUsecaseInterface } from '@Usecases/practitionerBox/getRecurringPractitionerBox.usecase';
 import { PractitionerAndBox } from '@Domains/PractitionerAndBox';
 import { PractitionerBox } from '@Domains/PractitionerBox';
 import { PractitionerBoxController } from './practitionerBox.controller';
-import { Product } from '@Domains/Product';
 import { ReturnValueType } from '@Filters/customError';
 import { UpdateRecurringPractitionerBoxesUsecaseInterface } from '@Usecases/practitionerBox/updateRecurringPractitionerBoxes.usecase';
 
@@ -34,14 +31,6 @@ describe('PractitionerBoxController', () => {
       description: '',
       note: '',
       products: [],
-    },
-  ];
-  const mockBoxProduct: Product[] = [
-    {
-      id: 1,
-      name: '',
-      label: '',
-      sku: '',
     },
   ];
 
@@ -71,13 +60,6 @@ describe('PractitionerBoxController', () => {
           } as GetPractitionerBoxByLabelUsecaseInterface,
         },
         {
-          provide: 'GetAllPractitionerBoxesUsecaseInterface',
-          useValue: {
-            getAllPractitionerBoxes: () =>
-              Promise.resolve<ReturnValueType<PractitionerBox[]>>([mockBoxPractitionerBox]),
-          } as GetAllPractitionerBoxesUsecaseInterface,
-        },
-        {
           provide: 'UpdateRecurringPractitionerBoxesUsecaseInterface',
           useValue: {
             filterDuplicatePractitionerBox: () =>
@@ -87,13 +69,6 @@ describe('PractitionerBoxController', () => {
             updateRecurringPractitionerBoxes: () =>
               Promise.resolve<ReturnValueType<PractitionerBox[]>>([mockBoxPractitionerBox]),
           } as UpdateRecurringPractitionerBoxesUsecaseInterface,
-        },
-        {
-          provide: 'GetAllProductsUsecaseInterface',
-          useValue: {
-            getAllProducts: () =>
-              Promise.resolve<ReturnValueType<Product[]>>([mockBoxProduct]),
-          } as GetAllProductsUsecaseInterface,
         },
         {
           provide: 'GetRecurringPractitionerBoxUsecaseInterface',
