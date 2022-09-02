e873ca32290dec2bc1227a9562b205d9c8f4ab04
Sho-ki <shoki0116.highjump@gmail.com>
2022-09-02 14:25:13 +0900

    add redirect-url in post-purchase

--- src/usecases/utils/practitionerBoxPlan.ts
+++ src/usecases/utils/practitionerBoxPlan.ts
@@ -3,4 +3,5 @@ export const PRACTITIONER_BOX_PLANS =
       merchandiseId: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zOTc2ODM3Nzg1MTk1OQ==',
       sellingPlanId: 'Z2lkOi8vc2hvcGlmeS9TZWxsaW5nUGxhbi81NjEwNTM3NTE=',
       id: 6666539204663,
+      sku: 'NP-influencer-curate-box',
     };
