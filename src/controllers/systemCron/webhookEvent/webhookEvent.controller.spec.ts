2a4b46a2f61535c237bc21440fffddaf745e270a
Sho-ki <shoki0116.highjump@gmail.com>
2022-10-03 14:25:49 -0700

    fix test

--- src/controllers/systemCron/webhookEvent/webhookEvent.controller.spec.ts
+++ src/controllers/systemCron/webhookEvent/webhookEvent.controller.spec.ts
@@ -1,16 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Status } from '@Domains/Status';
+import { ReturnValueType } from '../../../filter/customError';
+import { CheckUpdateOrderUsecaseInterface } from '@Usecases/webhookEvent/checkUpdateOrder.usecase';
 import { WebhookEventController } from './webhookEvent.controller';
 
 describe('WebhookEventController', () => {
   let controller: WebhookEventController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({ controllers: [WebhookEventController] }).compile();
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [WebhookEventController],
+      providers: [
+        {
+          provide: 'CheckUpdateOrderUsecaseInterface',
+          useValue: {
+            checkUpdateOrder: () =>
+              Promise.resolve<ReturnValueType<Status>>([{ success: true }]),
+          } as CheckUpdateOrderUsecaseInterface,
+        },
+      ],
+    }).compile();
 
     controller = module.get<WebhookEventController>(WebhookEventController);
   });
 
-  it('should be defined', () => {
+  it('should be defined', async () => {
     expect(controller).toBeDefined();
   });
 });
