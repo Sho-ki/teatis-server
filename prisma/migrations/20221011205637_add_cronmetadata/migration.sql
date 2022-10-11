740fa600735423bb056ad44678ac95813bac7ba7
Sho-ki <shoki0116.highjump@gmail.com>
2022-10-11 15:00:31 -0700

    update unique with apiId and cronMetadataName

--- prisma/migrations/20221011205637_add_cronmetadata/migration.sql
+++ prisma/migrations/20221011205637_add_cronmetadata/migration.sql
@@ -13,7 +13,7 @@ CREATE TABLE "CronMetadata" (
 CREATE TABLE "WebhookEvents" (
     "id" SERIAL NOT NULL,
     "apiId" TEXT NOT NULL,
-    "cronMetadataId" INTEGER NOT NULL,
+    "cronMetadataName" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -24,7 +24,7 @@ CREATE TABLE "WebhookEvents" (
 CREATE UNIQUE INDEX "CronMetadata_name_key" ON "CronMetadata"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "WebhookEvents_apiId_key" ON "WebhookEvents"("apiId");
+CREATE UNIQUE INDEX "WebhookEvents_apiId_cronMetadataName_key" ON "WebhookEvents"("apiId", "cronMetadataName");
 
 -- AddForeignKey
-ALTER TABLE "WebhookEvents" ADD CONSTRAINT "WebhookEvents_cronMetadataId_fkey" FOREIGN KEY ("cronMetadataId") REFERENCES "CronMetadata"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "WebhookEvents" ADD CONSTRAINT "WebhookEvents_cronMetadataName_fkey" FOREIGN KEY ("cronMetadataName") REFERENCES "CronMetadata"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
