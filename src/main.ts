fd4484d9f55796e86ee786c5941f8e66b59bd1d7
Sho-ki <76525425+Sho-ki@users.noreply.github.com>
2022-10-17 12:40:32 -0700

    Revert "install new relic"

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as Sentry from '@sentry/node';
-import { NewrelicInterceptor } from './newrelic.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -11,7 +10,6 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }),
   );
-  app.useGlobalInterceptors(new NewrelicInterceptor());
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
