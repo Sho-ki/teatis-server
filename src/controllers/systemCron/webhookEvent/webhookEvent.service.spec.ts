b076d3496b983770e585f0f93cdfd5dc7aa6102a
Sho-ki <shoki0116.highjump@gmail.com>
2022-12-06 19:15:23 -0800

    test fix

--- src/controllers/systemCron/webhookEvent/webhookEvent.service.spec.ts
+++ src/controllers/systemCron/webhookEvent/webhookEvent.service.spec.ts
@@ -1,32 +1,32 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { Status } from '@Domains/Status';
-// import { ReturnValueType } from '../../../filter/customError';
-// import { CheckUpdateOrderUsecaseInterface } from '@Usecases/webhookEvent/checkUpdateOrder.usecase';
-// import { WebhookEventService } from './webhookEvent.service';
-// import { Logger } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Status } from '@Domains/Status';
+import { ReturnValueType } from '../../../filter/customError';
+import { CheckUpdateOrderUsecaseInterface } from '@Usecases/webhookEvent/checkUpdateOrder.usecase';
+import { WebhookEventService } from './webhookEvent.service';
+import { Logger } from '@nestjs/common';
 
-// describe('WebhookEventService', () => {
-//   let controller: WebhookEventService;
+describe('WebhookEventService', () => {
+  let controller: WebhookEventService;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [WebhookEventService],
-//       providers: [
-//         Logger,
-//         {
-//           provide: 'CheckUpdateOrderUsecaseInterface',
-//           useValue: {
-//             checkUpdateOrder: () =>
-//               Promise.resolve<ReturnValueType<Status>>([{ success: true }]),
-//           } as CheckUpdateOrderUsecaseInterface,
-//         },
-//       ],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [WebhookEventService],
+      providers: [
+        Logger,
+        {
+          provide: 'CheckUpdateOrderUsecaseInterface',
+          useValue: {
+            checkUpdateOrder: () =>
+              Promise.resolve<ReturnValueType<Status>>([{ success: true }]),
+          } as CheckUpdateOrderUsecaseInterface,
+        },
+      ],
+    }).compile();
 
-//     controller = module.get<WebhookEventService>(WebhookEventService);
-//   });
+    controller = module.get<WebhookEventService>(WebhookEventService);
+  });
 
-//   it('should be defined', async () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+  it('should be defined', async () => {
+    expect(controller).toBeDefined();
+  });
+});
--- src/controllers/systemCron/webhookEvent/webhookEvent.service.ts
+++ src/controllers/systemCron/webhookEvent/webhookEvent.service.ts
@@ -1,24 +1,24 @@
-// import {  Inject, Injectable, Logger } from '@nestjs/common';
-// import { Cron } from '@nestjs/schedule';
+import {  Inject, Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 
-// import { CheckUpdateOrderUsecaseInterface } from '@Usecases/webhookEvent/checkUpdateOrder.usecase';
+import { CheckUpdateOrderUsecaseInterface } from '@Usecases/webhookEvent/checkUpdateOrder.usecase';
 
-// @Injectable()
-// export class WebhookEventService {
-//   constructor(
-//     @Inject('CheckUpdateOrderUsecaseInterface')
-//     private checkUpdateOrderUsecase: CheckUpdateOrderUsecaseInterface,
-//     private logger: Logger
-//   ) {}
+@Injectable()
+export class WebhookEventService {
+  constructor(
+    @Inject('CheckUpdateOrderUsecaseInterface')
+    private checkUpdateOrderUsecase: CheckUpdateOrderUsecaseInterface,
+    private logger: Logger
+  ) {}
 
-//   // Every 3 hours
-//   @Cron('0 */3 * * *')
-//   async checkUpdateOrderWebhook() {
-//     const [usecaseResponse, error] =
-//       await this.checkUpdateOrderUsecase.checkUpdateOrder();
-//     if (error) {
-//       this.logger.log('ERROR ', error);
-//     }
-//     this.logger.log('Successfully executed ', usecaseResponse);
-//   }
-// }
+  // Every 3 hours
+  @Cron('0 */3 * * *')
+  async checkUpdateOrderWebhook() {
+    const [usecaseResponse, error] =
+      await this.checkUpdateOrderUsecase.checkUpdateOrder();
+    if (error) {
+      this.logger.log('ERROR ', error);
+    }
+    this.logger.log('Successfully executed ', usecaseResponse);
+  }
+}
