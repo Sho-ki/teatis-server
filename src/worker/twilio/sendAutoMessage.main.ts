1f5e1f57efbf309811caddf3f329cea0c2de9f83
Sho-ki <shoki0116.highjump@gmail.com>
2022-12-29 23:22:01 -0800

    change maints

--- src/worker/twilio/sendAutoMessage.main.ts
+++ src/worker/twilio/sendAutoMessage.main.ts
@@ -5,10 +5,12 @@ import { SendAutoMessageService } from './sendAutoMessage.service';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
-exports.sendAutoMessage = async() => {
+const sendAutoMessage = async() => {
   const workerApp = await NestFactory.createApplicationContext(WorkerModule);
   const appService = workerApp.get(SendAutoMessageService);
   appService.sendAutoMessage();
   await workerApp.close();
 
 };
+
+exports.module = sendAutoMessage;
