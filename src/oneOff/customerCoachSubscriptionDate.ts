13c5afcdc6269d065581a7608a4ddab8550dd6d9
Sho-ki <shoki0116.highjump@gmail.com>
2022-12-29 22:09:47 -0800

    add one off

--- src/oneOff/customerCoachSubscriptionDate.ts
+++ src/oneOff/customerCoachSubscriptionDate.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -10,22 +11,53 @@ const updateCustomerCoachSubscriptionDate = async() => {
     { where: { coachId: 1 } });
 
   const serviceId = process.env.TWILIO_SERVICE_SID;
-  const twilioChannelUrl = `https://chat.twilio.com/v2/Services/${serviceId}/Channels?PageSize=200`;
   const username = process.env.TWILIO_ACCOUNT_SID;
   const password = process.env.TWILIO_AUTH_TOKEN;
 
-  const tst = await axios.get(twilioChannelUrl, { auth: { username, password } });
-  // eslint-disable-next-line no-console
-  console.log(tst.data.channels[2].friendly_name);
-  return;
-  for(const customer of response){
-    await client.customerEventLog.createMany(
-      { data: [{ customerId: customer.id, type: 'boxSubscribed', eventDate: customer.createdAt <= new Date('2022-12-06')?new Date('2022-12-06'):customer.createdAt }, { customerId: customer.id, type: 'coachingSubscribed', eventDate: customer.createdAt <= new Date('2022-12-06')?new Date('2022-12-06'):customer.createdAt }] });
-    await client.customers.update(
-      {
-        where: { id: customer.id },
-        data: { coachingSubscribed: 'active', boxSubscribed: 'active' },
-      });
+  const twilioChannelUrl = `https://chat.twilio.com/v2/Services/${serviceId}/Channels?PageSize=200`;
+
+  const data = await axios.get(twilioChannelUrl, { auth: { username, password } });
+  const list = data.data.channels.map(({ friendly_name, sid }) => { return { friendly_name, sid }; });
+
+  for(let i = 0; i < response.length; i++){
+
+    const rechargeUrl = 'https://api.rechargeapps.com//customers?email=' + response[i].email;
+    const rechargeData = await axios.get(rechargeUrl, { headers: { 'X-Recharge-Access-Token': 'sk_1x1_828119b467d40b545070d8d016d15061882f6d44cc0c6b5756a0f9d6e27dd5cb' } });
+    console.log(response[i].email);
+
+    if(!rechargeData.data.customers.length) continue;
+    console.log(rechargeData.data.customers[0].status);
+    const status = rechargeData.data.customers[0].status;
+    console.log(status);
+
+    if(status === 'INACTIVE'){
+      await client.customers.update(
+        {
+          where: { id: response[i].id },
+          data: { boxSubscribed: 'inactive', coachingSubscribed: 'inactive' },
+        });
+      await client.customerEventLog.createMany(
+        { data: [{ customerId: response[i].id, type: 'boxUnsubscribed', eventDate: new Date() }, { customerId: response[i].id, type: 'coachingUnsubscribed', eventDate: new Date() }] });
+
+      console.log('OK');
+    }
+    const customerName = `${response[i].firstName} ${response[i].lastName}`;
+    console.log(customerName);
+
+    const channel = list.find(({ friendly_name }) => { return friendly_name === customerName; });
+    if(channel){
+      const channelSid = channel.sid;
+      await client.customerEventLog.createMany(
+        { data: [{ customerId: response[i].id, type: 'boxSubscribed', eventDate: response[i].createdAt <= new Date('2022-12-06')?new Date('2022-12-06'):response[i].createdAt }, { customerId: response[i].id, type: 'coachingSubscribed', eventDate: response[i].createdAt <= new Date('2022-12-06')?new Date('2022-12-06'):response[i].createdAt }] });
+      await client.customers.update(
+        {
+          where: { id: response[i].id },
+          data: { twilioChannelSid: channelSid },
+        });
+    }
+
+    console.log(i);
+    console.log('_______________________________________________');
   }
 };
 
