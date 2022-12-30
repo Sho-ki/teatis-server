e79c080a240610490d1b96492b32be37782a72bb
Sho-ki <shoki0116.highjump@gmail.com>
2022-12-30 13:11:04 -0800

    run test

--- src/worker/twilio/sendAutoMessage.ts
+++ src/worker/twilio/sendAutoMessage.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { NestFactory } from '@nestjs/core';
 import { WorkerModule } from '../worker.module';
 import { SendAutoMessageService } from './sendAutoMessage.service';
@@ -5,10 +6,11 @@ import { SendAutoMessageService } from './sendAutoMessage.service';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
-module.exports.sendAutoMessage = async(req, res) => {
+module.exports.sendAutoMessage = async() => {
   const workerApp = await NestFactory.createApplicationContext(WorkerModule);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const appService = workerApp.get(SendAutoMessageService);
-  appService.sendAutoMessage();
-  await workerApp.close();
-  res.end();
+  console.log('SUCCESS');
+  // appService.sendAutoMessage();
+  // await workerApp.close();
 };
