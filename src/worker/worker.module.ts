42d0a5d8a3719bf70de96c72cdfe1a275b55ac91
Sho-ki <76525425+Sho-ki@users.noreply.github.com>
2023-01-07 12:24:52 -0800

    Revert "Feature/auto message bitly "

--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -1,7 +1,6 @@
 import { Logger, Module } from '@nestjs/common';
 import { Twilio } from 'twilio';
 import { PrismaService } from '../prisma.service';
-import { BitlyRepository } from '../repositories/bitly/bitly.repository';
 import { AutoMessageRepository } from '../repositories/teatisDB/autoMessage/autoMessage.repository';
 import { CoachRepository } from '../repositories/teatisDB/coach/coach.repository';
 import { CustomerGeneralRepository } from '../repositories/teatisDB/customer/customerGeneral.repository';
@@ -22,10 +21,7 @@ import { SendAutoMessageService } from './twilio/sendAutoMessage.service';
           process.env.TWILIO_AUTH_TOKEN,);
       },
     },
-    {
-      provide: 'BitlyRepositoryInterface',
-      useClass: BitlyRepository,
-    },
+
     {
       provide: 'AutoMessageRepositoryInterface',
       useClass: AutoMessageRepository,
