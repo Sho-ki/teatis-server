db6a5b8de353b85a2418b5755242170ca8c2bc63
hiroshi-tsutsui <hiroshi.tsutsui415@gmail.com>
2023-02-15 09:00:10 -0800

    fix seed

--- defaultData/weeklyCheckin.ts
+++ defaultData/weeklyCheckin.ts
@@ -12,7 +12,40 @@ interface Question extends Omit<SurveyQuestion, 'id' | 'createdAt' | 'updatedAt'
   images?:Omit<SurveyQuestionImage, 'id' | 'createdAt' | 'updatedAt' | 'surveyQuestionId'>[];
   options:Omit<SurveyQuestionOption, 'id' | 'createdAt' | 'updatedAt' | 'surveyQuestionId'>[];
 }
-
+const bloodSugar = [
+  '~50',
+  '50-70',
+  '70-90',
+  '90-110',
+  '110-130',
+  '130-150',
+  '150-170',
+  '170-190',
+  '190-210',
+  '210-230',
+  '230-250',
+  '250-270',
+  '270-290',
+  '290+',
+];
+const waterAmount = Array.from({ length: 12 }, (_, i) => String(i + 1));
+const exerciseAmount = [
+  '0-30',
+  '30-60',
+  '60-90',
+  '90-120',
+  '120-150',
+  '150-180',
+  '180+',
+];
+const createOptions = (labels: string[]) => {
+  const options = labels.map(label => {
+    const [min, max] = label.split('-').map(parseFloat);
+    const value = isNaN(min) ? max : (isNaN(max) ? min : (min + max) / 2);
+    return { label, isArchived: false, value };
+  });
+  return options;
+};
 export const weeklyCheckIn:Survey[] = [
   {
     name: 'weeklyCheckIn',
@@ -20,97 +53,51 @@ export const weeklyCheckIn:Survey[] = [
     questions: [
       {
         displayOrder: 1,
-        name: 'glucoseLevel',
-        label: 'How often do you eat snacks?',
+        name: 'bloodSugarBeforeMeal',
+        label: 'What was your blood sugar in mg/dl before meal?',
         isRequired: false,
         isCustomerFeature: false,
-        hint: null,
+        hint: 'Please refer to your values from yesterday.',
         placeholder: null,
         responseType: 'single',
         parentSurveyQuestionId: null,
-        options: [
-          {
-            label: 'More than once a day',
-            value: null,
-            isArchived: false,
-          },
-          {
-            label: 'Once a day',
-            value: null,
-            isArchived: false,
-          },
-          {
-            label: 'Few times a week',
-            value: null,
-            isArchived: false,
-          },
-          {
-            label: 'Less than once a week',
-            value: null,
-            isArchived: false,
-          },
-        ],
+        options: createOptions(bloodSugar),
       },
       {
         displayOrder: 2,
-        name: 'waterAmount',
-        label: 'How much water did you drink on average this week?',
+        name: 'bloodSugarAfterMeal',
+        label: 'What was your blood sugar in mg/dl 2 hours after meal?',
         isRequired: false,
         isCustomerFeature: false,
-        hint: null,
+        hint: 'Please refer to your values from yesterday.',
         placeholder: null,
         responseType: 'single',
         parentSurveyQuestionId: null,
-        options: [
-          {
-            label: 'Less than 6 cups',
-            value: null,
-            isArchived: false,
-          },
-          {
-            label: '6~8 cups',
-            value: null,
-            isArchived: false,
-          },
-          {
-            label: 'More than 8 cups',
-            value: null,
-            isArchived: false,
-          },
-        ],
+        options: createOptions(bloodSugar),
       },
       {
         displayOrder: 3,
+        name: 'waterAmount',
+        label: 'How many cups of water did you drink on average this week?',
+        isRequired: false,
+        isCustomerFeature: false,
+        hint: `8oz per cup`,
+        placeholder: null,
+        responseType: 'single',
+        parentSurveyQuestionId: null,
+        options: createOptions(waterAmount),
+      },
+      {
+        displayOrder: 4,
         name: 'exerciseAmount',
-        label: 'How much time did you spend exercising this week?',
+        label: 'How much time in minutes did you spend exercising this week?',
         isRequired: false,
         isCustomerFeature: false,
         hint: null,
         placeholder: null,
         responseType: 'single',
         parentSurveyQuestionId: null,
-        options: [
-          {
-            label: 'More than once a day',
-            value: null,
-            isArchived: false,
-          },
-          {
-            label: 'Once a day',
-            value: null,
-            isArchived: false,
-          },
-          {
-            label: 'Few times a week',
-            value: null,
-            isArchived: false,
-          },
-          {
-            label: 'Less than once a week',
-            value: null,
-            isArchived: false,
-          },
-        ],
+        options: createOptions(exerciseAmount),
       },
     ],
   },
