22cb13a699f176cc87071f7fbcfe6fa8d9e60b9f
Sho-ki <shoki0116.highjump@gmail.com>
2023-02-15 18:03:55 -0800

    delete profile files and add customerType in pre-purchase request

--- src/controllers/discoveries/prePurchaseSurvey/dtos/postPrePurchaseSurvey.dto.ts
+++ src/controllers/discoveries/prePurchaseSurvey/dtos/postPrePurchaseSurvey.dto.ts
@@ -1,6 +1,14 @@
-import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsArray, IsEnum, IsNumber, IsOptional, IsString, ValidateIf } from 'class-validator';
+
+enum CustomerType {
+  CUSTOMER = 'customer',
+  EMPLOYEE = 'employee',
+}
 
 export class PostPrePurchaseSurveyDto {
+  @IsEnum(CustomerType)
+    customerType: CustomerType;
+
   @IsNumber()
   @IsOptional()
     gender?: number;
@@ -19,4 +27,41 @@ export class PostPrePurchaseSurveyDto {
 
   @IsString()
     email: string;
+
+  @ValidateIf(o => o.customerType === CustomerType.EMPLOYEE)
+  @IsString()
+    phoneNumber: string;
+
+  @ValidateIf(o => o.customerType === CustomerType.EMPLOYEE)
+  @IsString()
+    firstName: string;
+
+  @ValidateIf(o => o.customerType === CustomerType.EMPLOYEE)
+  @IsString()
+    lastName: string;
+
+  @ValidateIf(o => o.customerType === CustomerType.EMPLOYEE)
+  @IsString()
+    address1: string;
+
+  @ValidateIf(o => o.customerType === CustomerType.EMPLOYEE)
+  @IsString()
+  @IsOptional()
+    address2?: string;
+
+  @ValidateIf(o => o.customerType === CustomerType.EMPLOYEE)
+  @IsString()
+    city: string;
+
+  @ValidateIf(o => o.customerType === CustomerType.EMPLOYEE)
+  @IsString()
+    state: string;
+
+  @ValidateIf(o => o.customerType === CustomerType.EMPLOYEE)
+  @IsString()
+    zip: string;
+
+  @ValidateIf(o => o.customerType === CustomerType.EMPLOYEE)
+  @IsString()
+    country: string;
 }
