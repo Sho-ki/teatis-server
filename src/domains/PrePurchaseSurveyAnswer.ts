83c36b05aaaef9a441c9570b47579f0ae5a43657
Sho-ki <shoki0116.highjump@gmail.com>
2023-02-23 09:39:44 -0800

    hot fix

--- src/domains/PrePurchaseSurveyAnswer.ts
+++ src/domains/PrePurchaseSurveyAnswer.ts
@@ -3,13 +3,7 @@ export interface PrePurchaseSurveyAnswer{
     email: string;
     diabetes?: string;
     gender?: string;
-    height?: number; // in cm
-    weight?: number; // in kg
-    age?: number;
     medicalConditions?: { name: string, label?: string }[];
-    activeLevel?: string;
-    A1c?: string;
-    mealsPerDay?: number;
     categoryPreferences?: { name: string, label?: string }[];
     flavorDislikes?: { name: string, label?: string }[];
     ingredientDislikes?: { name: string, label?: string }[];
