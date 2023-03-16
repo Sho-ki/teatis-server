a35d0bac2877a2cedb24b5571ef2c2b51800b56d
hiroshi-tsutsui <hiroshi.tsutsui415@gmail.com>
2023-03-16 02:54:36 -0700

    get all messages in the last 24 hours. filter out messages from same person.

--- src/usecases/chatGPT/createCustomerConversationSummary.usecase.ts
+++ src/usecases/chatGPT/createCustomerConversationSummary.usecase.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ReturnValueType } from '@Filters/customError';
 import { TwilioRepository } from '../../repositories/twilio/twilio.repository';
+import { MessageInstance } from 'twilio/lib/rest/api/v2010/account/message';
 
 export interface CreateCustomerConversationSummaryUsecaseInterface {
     createCustomerConversationSummary(): Promise<ReturnValueType<unknown>>;
@@ -13,12 +14,20 @@ implements CreateCustomerConversationSummaryUsecaseInterface {
     @Inject('TwilioRepositoryInterface')
     private twilioRepository: TwilioRepository,
   ){}
-  async createCustomerConversationSummary(): Promise<ReturnValueType<unknown>> {
-    const [res, error] = await this.twilioRepository.getAllConversations();
-    return;
+  async createCustomerConversationSummary(): Promise<ReturnValueType<MessageInstance[]>> {
+    const [res, error] = await this.twilioRepository.getInboundConversations();
     if(error){
       return [undefined, error];
     }
-    return [{ success: true }];
+    const newMessages = res
+      .sort((a, b) => +new Date(b.dateUpdated) - +new Date(a.dateUpdated))
+      .filter((message, index, array) => {
+        // Check if this message has the same from and to phone numbers as any previous message
+        const hasDuplicate = array.slice(0, index).some(previousMessage => {
+          return previousMessage.from === message.from && previousMessage.to === message.to;
+        });
+        return !hasDuplicate;
+      });
+    return [newMessages];
   }
 }
