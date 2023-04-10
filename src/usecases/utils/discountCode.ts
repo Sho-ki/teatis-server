953696ecf6b1eed55b05d65e5fec4e01a83405cf
Sho-ki <shoki0116.highjump@gmail.com>
2023-04-09 18:39:44 -0700

    add discount code for drivers

--- src/usecases/utils/discountCode.ts
+++ src/usecases/utils/discountCode.ts
@@ -1,1 +1,4 @@
-export const DISCOUNT_CODES = { customerBox: { firstPurchase: 'A9RR7KM3HD23' } };
+export const DISCOUNT_CODES = {
+  customerBox: { firstPurchase: 'A9RR7KM3HD23' },
+  driver: { firstPurchase: 'RR7KM3H12' },
+};
