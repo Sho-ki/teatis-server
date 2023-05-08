5ffe94939316677bec53d0416231e556d4e9deb9
Sho-ki <shoki0116.highjump@gmail.com>
2023-05-08 12:01:01 -0700

    domain name update

---
+++ src/domains/CustomerMicroGoalWithActionSteps.ts
@@ -0,0 +1,19 @@
+import {
+  ActionStep,
+  ActionStepImage,
+  CustomerActionStep,
+  CustomerActionStepImage,
+  CustomerMicroGoal,
+  MicroGoal,
+  MicroGoalCategory,
+} from '@prisma/client';
+
+export interface CustomerMicroGoalWithActionSteps
+  extends CustomerMicroGoal,
+    Pick<MicroGoal, 'label'> {
+  category: MicroGoalCategory;
+  actionSteps: (ActionStep & { actionStepImage?: ActionStepImage[] })[];
+  customerActionSteps: (CustomerActionStep & {
+    customerActionStepImage?: CustomerActionStepImage[];
+  })[];
+}
--- src/repositories/teatisDB/microGoal/microGoal.respository.ts
+++ src/repositories/teatisDB/microGoal/microGoal.respository.ts
@@ -4,20 +4,24 @@ import { PrismaService } from '../../../prisma.service';
 import { Transactionable } from '../../utils/transactionable.interface';
 import { PrismaClient, Prisma } from '@prisma/client';
 import { ReturnValueType } from '../../../filter/customError';
-import { MicroGoalWithCategory } from '../../../domains/MicroGoalWithCategory';
+import { MicroGoalWithActionSteps } from '../../../domains/MicroGoalWithActionSteps';
 
 export interface MicroGoalRepositoryInterface extends Transactionable {
-  getMicroGoalsWithCategory():
-  Promise<ReturnValueType<MicroGoalWithCategory[]>>;
+  getMicroGoalsWithActionSteps(): Promise<
+    ReturnValueType<MicroGoalWithActionSteps[]>
+  >;
 }
 
 @Injectable()
-export class MicroGoalRepository
-implements MicroGoalRepositoryInterface
-{
-  private originalPrismaClient : PrismaClient;
-  constructor(@Inject(PrismaService) private prisma: PrismaClient | Prisma.TransactionClient) {}
-  setPrismaClient(prisma: Prisma.TransactionClient): MicroGoalRepositoryInterface {
+export class MicroGoalRepository implements MicroGoalRepositoryInterface {
+  private originalPrismaClient: PrismaClient;
+  constructor(
+    @Inject(PrismaService)
+    private prisma: PrismaClient | Prisma.TransactionClient,
+  ) {}
+  setPrismaClient(
+    prisma: Prisma.TransactionClient,
+  ): MicroGoalRepositoryInterface {
     this.originalPrismaClient = this.prisma as PrismaClient;
     this.prisma = prisma;
     return this;
@@ -27,19 +31,24 @@ implements MicroGoalRepositoryInterface
     this.prisma = this.originalPrismaClient;
   }
 
-  async getMicroGoalsWithCategory():
-    Promise<ReturnValueType<MicroGoalWithCategory[]>> {
-    const response = await this.prisma.microGoal.findMany(
-      { include: { subCategory: { include: { category: true } } } });
-
-    const microGoalsWithCategory:MicroGoalWithCategory[] = response.map((microGoal) => {
-      const { subCategory } = microGoal;
-      const { category } = subCategory;
-      delete subCategory.category;
-      return { ...microGoal, subCategory, category };
+  async getMicroGoalsWithActionSteps(): Promise<
+    ReturnValueType<MicroGoalWithActionSteps[]>
+    > {
+    const response = await this.prisma.microGoal.findMany({
+      include: {
+        actionSteps: { include: { actionStepImage: { orderBy: { position: 'asc' } } } },
+        subCategory: { include: { category: true } },
+      },
     });
+    const microGoalsWithCategory: MicroGoalWithActionSteps[] = response.map(
+      (microGoal) => {
+        const { subCategory, actionSteps } = microGoal;
+        const { category } = subCategory;
+        delete subCategory.category;
+        return { ...microGoal, subCategory, category, actionSteps };
+      },
+    );
 
     return [microGoalsWithCategory];
   }
-
 }
