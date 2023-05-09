b46c0b2a814ad88b4739d5342b2aa7fbba6884cd
Sho-ki <shoki0116.highjump@gmail.com>
2023-05-09 13:06:44 -0700

    update eslint

--- src/domains/MicroGoalWithCategory.ts
+++
@@ -1,6 +0,0 @@
-import { MicroGoal, MicroGoalCategory, MicroGoalSubCategory } from '@prisma/client';
-
-export interface MicroGoalWithCategory extends MicroGoal {
-    subCategory: MicroGoalSubCategory;
-    category: MicroGoalCategory;
-}
